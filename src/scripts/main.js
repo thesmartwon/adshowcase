a3867fe244978de06503444582488f9c26fc1072
thesmartwon <thesmartwon@live.com>
2020-09-21 17:19:22 -0400

    add ci

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-const galleries = Array.from(document.getElementsByClassName('gallery'));
+const ads = Array.from(document.getElementsByClassName('adsbygoogle'));
+
+// const pageOptions = {
+//   pubId: 'pub-9616389000213823', // Make sure this the correct client ID!
+//   query: 'hotels',
+//   adPage: 1
+// };
+
+// ads.map(el => el.id).forEach(id => {
+//   _googCsa(
+//     'ads',
+//     pageOptions,
+//     {
+//       container: id,
+//       width: 700,
+//       number: id + 1
+//     }
+//   );
+// });
+for (let i = 0; i < 20; i++) {
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
